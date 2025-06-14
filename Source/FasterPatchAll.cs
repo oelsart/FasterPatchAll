aa571106c8f3bb4bfcf09eb4e091f377a8c06db2
OELS <oelsart@gmail.com>
2025-06-14 22:44:55 +0900

    早期フィルタリングはマジで意味なかったので削除

--- Source/FasterPatchAll.cs
+++ Source/FasterPatchAll.cs
@@ -18,14 +18,11 @@ namespace FasterPatchAll
 
         public Dictionary<Assembly, Type[]> TypesByAssembly;
 
-        private HashSet<string> HarmonyAttributeNames;
-
         public FasterPatchAll(ModContentPack content) : base(content)
         {
             Mod = this;
             Settings = GetSettings<Settings>();
 
-            HarmonyAttributeNames = typeof(HarmonyAttribute).AllSubclasses().Concat(typeof(HarmonyAttribute)).Select(a => a.FullName).ToHashSet();
             if (Settings.CacheTypesByAssembly)
             {
                 TypesByAssembly = GenTypes.AllTypes.GroupBy(t => t.Assembly).ToDictionary(g => g.Key, g => g.ToArray());
@@ -54,25 +51,18 @@ namespace FasterPatchAll
             var listing_Standard = new Listing_Standard();
             listing_Standard.Begin(inRect.BottomPartPixels(inRect.height - 50f));
             listing_Standard.CheckboxLabeled("Cache types by assembly.", ref Settings.CacheTypesByAssembly);
-            listing_Standard.CheckboxLabeled("Early harmony class filtering.", ref Settings.EarlyFiltering);
             listing_Standard.End();
         }
 
         public void Clear()
         {
             TypesByAssembly = null;
-            HarmonyAttributeNames = null;
             Harmony.UnpatchAll("OELS.FasterPatchAll");
         }
 
         public override string SettingsCategory()
         {
             return "FasterPatchAll()";
-        }
-
-        public static bool CanBeHarmonyClass(Type type)
-        {
-            return type?.CustomAttributes.Any(data => Mod.HarmonyAttributeNames?.Contains(data.AttributeType.FullName) ?? true) ?? false;
         }
     }
 
@@ -94,79 +84,12 @@ namespace FasterPatchAll
         }
     }
 
-    [HarmonyPatch(typeof(Harmony), nameof(Harmony.PatchAll), typeof(Assembly))]
-    internal class Patch_Harmony_PatchAll
-    {
-        static bool Prepare()
-        {
-            return FasterPatchAll.Mod.Settings.EarlyFiltering;
-        }
-
-        static bool Prefix(Harmony __instance, Assembly assembly)
-        {
-            AccessTools.GetTypesFromAssembly(assembly)
-                .Where(FasterPatchAll.CanBeHarmonyClass)
-                .Do(type =>
-                {
-                    __instance.CreateClassProcessor(type).Patch();
-                });
-            return false;
-        }
-    }
-
-
-    [HarmonyPatch(typeof(Harmony), nameof(Harmony.PatchCategory), typeof(Assembly), typeof(string))]
-    internal class Patch_Harmony_PatchCategory
-    {
-        static bool Prepare()
-        {
-            return FasterPatchAll.Mod.Settings.EarlyFiltering;
-        }
-
-        static bool Prefix(Harmony __instance, Assembly assembly, string category)
-        {
-            AccessTools.GetTypesFromAssembly(assembly)
-                .Where(FasterPatchAll.CanBeHarmonyClass)
-                .Where(type =>
-                {
-                    List<HarmonyMethod> fromType = HarmonyMethodExtensions.GetFromType(type);
-                    HarmonyMethod harmonyMethod = HarmonyMethod.Merge(fromType);
-                    return harmonyMethod.category == category;
-                }).Do(type =>
-                {
-                    __instance.CreateClassProcessor(type).Patch();
-                });
-            return false;
-        }
-    }
-
-    [HarmonyPatch(typeof(Harmony), nameof(Harmony.PatchAllUncategorized), typeof(Assembly))]
-    internal class Patch_Harmony_PatchAllUncategorized
-    {
-        static bool Prepare()
-        {
-            return FasterPatchAll.Mod.Settings.EarlyFiltering;
-        }
-
-        static bool Prefix(Harmony __instance, Assembly assembly)
-        {
-            AccessTools.GetTypesFromAssembly(assembly)
-                .Where(FasterPatchAll.CanBeHarmonyClass)
-                .Select(__instance.CreateClassProcessor)
-                .DoIf(patchClass => string.IsNullOrEmpty(patchClass.Category), patchClass =>
-                {
-                    patchClass.Patch();
-                });
-            return false;
-        }
-    }
-
     [HarmonyPatch(typeof(StaticConstructorOnStartupUtility), nameof(StaticConstructorOnStartupUtility.CallAll))]
     internal class Patch_StaticConstructorOnStartupUtility_CallAll
     {
         static bool Prepare()
         {
-            return FasterPatchAll.Mod.Settings.CacheTypesByAssembly || FasterPatchAll.Mod.Settings.EarlyFiltering;
+            return FasterPatchAll.Mod.Settings.CacheTypesByAssembly;
         }
 
         static void Postfix()
--- Source/Settings.cs
+++ Source/Settings.cs
@@ -6,12 +6,9 @@ namespace FasterPatchAll
     {
         public bool CacheTypesByAssembly = true;
 
-        public bool EarlyFiltering = false;
-
         public override void ExposeData()
         {
             Scribe_Values.Look(ref CacheTypesByAssembly, "CacheTypesByAssembly", true);
-            Scribe_Values.Look(ref EarlyFiltering, "EarlyFiltering", false);
         }
     }
 }
